30e27c87231019ac4901f72c7b7159a5067dcf55
Victor Hugo <victor.nunes@kobold.com.br>
2023-11-02 23:33:10 -0300

    Adiciona camada domain

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,41 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name) 
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid Name. Name is invalid.");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid Name. Name is too short.");
+        }
+    }
+
+ 
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int? id);
+        Task<Product> GetProductByCategoryIdAsync(int? id);
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}
