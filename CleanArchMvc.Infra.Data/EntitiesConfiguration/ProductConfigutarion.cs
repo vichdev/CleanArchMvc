335084bc7e265e2d62fbd5ac7306f5715d67ffc4
Victor Hugo <victor.nunes@kobold.com.br>
2023-11-07 23:40:48 -0300

    Implementa camada Infra.Data

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfigutarion.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfigutarion : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(property => property.Name).HasMaxLength(100).IsRequired();
+            builder.Property(property => property.Description).HasMaxLength(200).IsRequired();
+            builder.Property(property => property.Price).HasPrecision(10, 2);
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
